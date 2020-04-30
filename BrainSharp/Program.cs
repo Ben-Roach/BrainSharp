2bb25178c325ed87dffd21864e76b086e0509118
Ben Roach <benroach123@gmail.com>
2020-04-29 17:14:39 -0700

    Fixed bugs, improved functionality. Now only reads from files, has command line switches

--- BrainSharp/Program.cs
+++ BrainSharp/Program.cs
@@ -25,110 +25,120 @@ namespace Brainsharp
         public byte[] data_array { get; private set; } // data array
         public int src_line { get; private set; } // current source line
         public int src_col { get; private set; } // current source column
+        public string instructions { get; private set; } // instructions
+        public int step { get; private set; }
 
-        public Interpreter(int array_size=30000)
+        public Interpreter(string instructions, int array_size=30000)
         {
             this.dp = 0;
             this.ip = 0;
+            this.step = 0;
             this.src_line = 0;
             this.src_col = 0;
             this.data_array = new byte[array_size];
+            this.instructions = instructions;
         }
 
-        public void Execute(string instructions)
+        public void Execute(int steps=0)
         {
-            while (ip < instructions.Length)
+            while (ip < instructions.Length && (steps == 0 || step < steps))
             {
-                switch (instructions[ip])
-                {
-                    // execute instructions
-                    case '>':
-                        dp++;
-                        break;
-                    case '<':
-                        dp--;
-                        break;
-                    case '+':
-                        data_array[dp]++;
-                        break;
-                    case '-':
-                        data_array[dp]--;
-                        break;
-                    case '.':
-                        Console.Write((char)data_array[dp]);
-                        break;
-                    case ',':
-                        data_array[dp] = (byte)Console.ReadKey().KeyChar;
-                        break;
-                    case '[':
-                        if (data_array[dp] == 0)
-                        {
-                            int skip = 0; // increment with [, decrement with ]
-                            bool found = false;
-
-                            for (int i = ip + 1; i < instructions.Length; i++)
-                            {
-                                if (instructions[i] == '[')
-                                    skip++;
-                                else if (instructions[i] == '[' && skip > 0)
-                                    skip--;
-                                else if (instructions[i] == ']' && skip == 0)
-                                {
-                                    ip = i;
-                                    found = true;
-                                    break;
-                                }
-                            }
-                            if (!found)
-                                throw new BrainFuckExecutionException(FormatDebugLine("Matching ']' not found for '['"));
-                        }
-                        break;
-                    case ']':
-                        if (data_array[dp] != 0)
-                        {
-                            int skip = 0; // increment with ], decrement with [
-                            bool found = false;
-
-                            for (int i = ip - 1; i >= 0; i--)
-                            {
-                                if (instructions[i] == ']')
-                                    skip++;
-                                else if (instructions[i] == '[' && skip > 0)
-                                    skip--;
-                                else if (instructions[i] == '[' && skip == 0)
-                                {
-                                    ip = i;
-                                    found = true;
-                                    break;
-                                }
-                            }
-                            if (!found)
-                                throw new BrainFuckExecutionException(FormatDebugLine("Matching '[' not found for ']'"));
-                        }
-                        break;
-                    // track line number
-                    case '\n':
-                        src_line++;
-                        src_col = -1;
-                        break;
-                    default:
-                        break;
-                }
-                src_col++;
-                ip++;
+                Step();
             }
+        }
+
+        public void Step()
+        {
+            switch (instructions[ip])
+            {
+                // execute instructions
+                case '>':
+                    dp++;
+                    break;
+                case '<':
+                    dp--;
+                    break;
+                case '+':
+                    data_array[dp]++;
+                    break;
+                case '-':
+                    data_array[dp]--;
+                    break;
+                case '.':
+                    Console.Write((char)data_array[dp]);
+                    break;
+                case ',':
+                    data_array[dp] = (byte)Console.ReadKey().KeyChar;
+                    break;
+                case '[':
+                    if (data_array[dp] == 0)
+                    {
+                        int skip = 0; // increment with [, decrement with ]
+                        bool found = false;
+
+                        for (int i = ip + 1; i < instructions.Length; i++)
+                        {
+                            if (instructions[i] == '[')
+                                skip++;
+                            else if (instructions[i] == ']' && skip > 0)
+                                skip--;
+                            else if (instructions[i] == ']' && skip == 0)
+                            {
+                                ip = i;
+                                found = true;
+                                break;
+                            }
+                        }
+                        if (!found)
+                            throw new BrainFuckExecutionException(FormatDebugLine("Matching ']' not found for '['"));
+                    }
+                    break;
+                case ']':
+                    if (data_array[dp] != 0)
+                    {
+                        int skip = 0; // increment with ], decrement with [
+                        bool found = false;
+
+                        for (int i = ip - 1; i >= 0; i--)
+                        {
+                            if (instructions[i] == ']')
+                                skip++;
+                            else if (instructions[i] == '[' && skip > 0)
+                                skip--;
+                            else if (instructions[i] == '[' && skip == 0)
+                            {
+                                ip = i;
+                                found = true;
+                                break;
+                            }
+                        }
+                        if (!found)
+                            throw new BrainFuckExecutionException(FormatDebugLine("Matching '[' not found for ']'"));
+                    }
+                    break;
+                // track line number
+                case '\n':
+                    src_line++;
+                    src_col = -1; // incremented before next step
+                    break;
+                default:
+                    break;
+            }
+            src_col++;
+            step++;
+            ip++;
         }
 
         private string FormatDebugLine(string message)
         {
-            return String.Format("[{0}, {1}]: {2}", src_line, src_col, message);
+            return string.Format("[{0}, {1}]: {2}", src_line, src_col, message);
         }
     }
 
-    public class Minifier
+    public static class Minifier
     {
-        private readonly char[] allowedChars = new char[] { '<', '>', '+', '-', '.', ',', '[', ']' };
-        public string Minify(string source)
+        private static readonly char[] allowedChars = new char[] { '<', '>', '+', '-', '.', ',', '[', ']' };
+        public static string Minify(string source)
         {
             StringBuilder sb = new StringBuilder();
             foreach (char c in source)
@@ -151,16 +161,32 @@ class Program
         // step-by-step execution?
         // verbose mode?
 
-        // get filename
-        string code;
-        if (args.Length > 0)
-            code = File.ReadAllText(args[0]);
+        // execute mode (-e <filepath>)
+        if (args.Length == 2 && args[0] == "-e")
+        {
+            string code;
+            try
+            {
+                code = File.ReadAllText(args[1]);
+            }
+            catch
+            {
+                Console.WriteLine("Error reading file. Aborting.");
+                return;
+            }
+            Brainsharp.Interpreter interpreter = new Brainsharp.Interpreter(code);
+            interpreter.Execute();
+            Console.ReadLine();
+        }
+        // no match, show help
         else
-            Console.WriteLine("Enter code to execute:");
-        code = Console.ReadLine();
+        {
+            Console.WriteLine(
+                @"Usage:
+                BrainSharp.exe -e <filepath>    Execute a Brainfuck file");
+            return;
+        }
 
-        Brainsharp.Interpreter interpreter = new Brainsharp.Interpreter();
-        interpreter.Execute(code);
-        Console.ReadLine();
+        
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # BrainSharp
-A set of BrainFuck tools for C#.
+A set of BrainFuck development and execution tools for C#.
+
+### Have you ever wanted to develop your apps in BrainFuck?
+
+### No?
+
+### Fuck.
+
+### Well, regardless, I wrote this. I call it BrainSharp.
