5b863f7f0c8139cc4333e236ceafdcf911c11440
Ben Roach <benroach123@gmail.com>
2020-04-29 19:01:37 -0700

    minify did the exact opposite of what it was supposed to do. Fixed

--- BrainSharp/Program.cs
+++ BrainSharp/Program.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 /* BrainSharp
  * By Benjamin Roach
- * 
+ * https://github.com/Ben-Roach/BrainSharp
  */
 
 namespace BrainSharp
@@ -18,27 +18,42 @@ namespace BrainSharp
         public BrainfuckExecutionException(string message, Exception inner) : base(message, inner) { }
     }
 
+    /* A BF interpreter.
+     */
     public class Interpreter
     {
-        public int dp { get; private set; } // data pointer
-        public int ip { get; private set; } // instruction pointer
-        public byte[] data_array { get; private set; } // data array
-        public int src_line { get; private set; } // current source line
-        public int src_col { get; private set; } // current source column
-        public string instructions { get; private set; } // instructions
+        // data pointer index
+        public int dp { get; private set; }
+        // instruction pointer index
+        public int ip { get; private set; }
+        // current number of steps completed
         public int step { get; private set; }
+        // data array
+        public byte[] data_array { get; private set; }
+        // current source line
+        public int src_line { get; private set; }
+        // BF code (and maybe comments)
+        public string instructions { get; private set; }
+        // current source column
+        public int src_col { get; private set; }
 
-        public Interpreter(string instructions, int array_size=30000)
+        /* Initializes the interpreter. Must give it BF code as a string,
+         * and may specify any desired settings. Note that including comments/
+         * non-BF instructions is ok, but will slow down execution.
+         */
+        public Interpreter(string code, int array_size=30000, bool verbose=false)
         {
             this.dp = 0;
             this.ip = 0;
             this.step = 0;
             this.src_line = 0;
             this.src_col = 0;
+            this.instructions = code;
             this.data_array = new byte[array_size];
-            this.instructions = instructions;
         }
 
+        /* Executes the BF code.
+         */
         public void Execute(int steps=0)
         {
             while (ip < instructions.Length && (steps == 0 || step < steps))
@@ -116,7 +131,7 @@ namespace BrainSharp
                             throw new BrainfuckExecutionException(FormatDebugLine("Matching '[' not found for ']'"));
                     }
                     break;
-                // track line number
+                // update line number
                 case '\n':
                     src_line++;
                     src_col = -1; // incremented before next step
@@ -124,6 +139,7 @@ namespace BrainSharp
                 default:
                     break;
             }
+            // update column number, step, and instruction pointer
             src_col++;
             step++;
             ip++;
@@ -135,18 +151,78 @@ namespace BrainSharp
         }
     }
 
+    /* Contains tools for minifying BF code.
+     */
     public static class Minifier
     {
         private static readonly char[] allowedChars = new char[] { '<', '>', '+', '-', '.', ',', '[', ']' };
-        public static string Minify(string source)
+        // Gets a minified version of the given BF code
+        public static string GetMinified(string source)
         {
             StringBuilder sb = new StringBuilder();
             foreach (char c in source)
             {
-                if (Array.IndexOf(allowedChars, c) == -1)
+                if (Array.IndexOf(allowedChars, c) != -1)
                     sb.Append(c);
             }
             return sb.ToString();
+        }
+    }
+
+    // Provides a command-line inteface for BrainSharp.
+    public static class BrainSharpCLI
+    {
+        /* execute a command that is an array of arguments.
+         */
+        public static void Execute(string[] args)
+        {
+            // execute mode (-e <filepath>)
+            if (args.Length >= 2 && args[0] == "-e")
+            {
+                string code;
+                try
+                {
+                    code = File.ReadAllText(args[1]);
+                }
+                catch
+                {
+                    Console.WriteLine("Error reading file. Aborting.");
+                    return;
+                }
+                Interpreter interpreter = new Interpreter(code);
+                interpreter.Execute();
+                Console.ReadLine();
+            }
+            // minify mode (-m <filepath>)
+            if (args.Length >= 2 && args[0] == "-m")
+            {
+                string code;
+                try
+                {
+                    code = File.ReadAllText(args[1]);
+                }
+                catch
+                {
+                    Console.WriteLine("Error reading file. Aborting.");
+                    return;
+                }
+                Console.WriteLine(Minifier.GetMinified(code));
+            }
+            // no match, show help
+            else
+            {
+                Console.WriteLine(@"Usage:
+  BrainSharp.exe -e <filepath>    Execute a Brainfuck file
+  BrainSharp.exe -m <filepath>    Minify a Brainfuck file");
+                return;
+            }
+        }
+
+        /* Execute a command that is a single string.
+         */
+        public static void Execute(string command)
+        {
+            Execute(command.Split(' '));
         }
     }
 }
@@ -158,35 +234,7 @@ class Program
         // larger data values than char?
         // allow flexible array length?
         // debug with line, col numbers
-        // step-by-step execution?
         // verbose mode?
-
-        // execute mode (-e <filepath>)
-        if (args.Length == 2 && args[0] == "-e")
-        {
-            string code;
-            try
-            {
-                code = File.ReadAllText(args[1]);
-            }
-            catch
-            {
-                Console.WriteLine("Error reading file. Aborting.");
-                return;
-            }
-            BrainSharp.Interpreter interpreter = new BrainSharp.Interpreter(code);
-            interpreter.Execute();
-            Console.ReadLine();
-        }
-        // no match, show help
-        else
-        {
-            Console.WriteLine(
-                @"Usage:
-                BrainSharp.exe -e <filepath>    Execute a Brainfuck file");
-            return;
-        }
-
-        
+        BrainSharp.BrainSharpCLI.Execute(args);
     }
 }
--- README.md
+++ README.md
@@ -3,8 +3,8 @@ A set of Brainfuck development and execution tools for C#.
 
 ### Have you ever wanted to develop your apps in Brainfuck?
 
-### No?
+**No?**
 
-### Fuck.
+**Fuck.**
 
 ### Well, regardless, I wrote this. I call it BrainSharp.
