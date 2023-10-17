c51190989655b7f374d0d7a60f8a58e608ccec03
Ben Roach <benroach123@gmail.com>
2023-10-17 00:11:07 -0700

    Add unit test project

---
+++ BrainSharpUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BrainSharpUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BrainSharpUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1f3cdd95-bf05-49f0-9fd4-2e6e8b2f3285")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BrainSharpUnitTest/UnitTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BrainSharpUnitTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        string TestSourcesPath = "../../TestSources/";
+
+
+        [TestMethod]
+        public void TestHelloWorld()
+        {
+            var testCases = new List<string>()
+            {
+                Path.Combine(TestSourcesPath, $"helloworld1.bf"),
+                Path.Combine(TestSourcesPath, $"helloworld2.bf"),
+                Path.Combine(TestSourcesPath, $"helloworld3.bf")
+            };
+
+            foreach (string testCase in testCases)
+            {
+                using (var istream = new StringReader(""))
+                using (var ostream = new StringWriter())
+                {
+                    string source = File.ReadAllText(testCase);
+                    var settings = new BrainSharp.InterpreterSettings();
+                    settings.InputStream = istream;
+                    settings.OutputStream = ostream;
+                    var interpreter = new BrainSharp.Interpreter(source, settings);
+                    interpreter.Execute();
+                    Assert.AreEqual("Hello World!\n", settings.OutputStream.ToString(), $"Input: {testCase} Output: {settings.OutputStream}");
+                }
+            }
+        }
+
+
+        [TestMethod]
+        public void TestRot13()
+        {
+            var testCases = new List<string>()
+            {
+                "Hello World!"
+            };
+
+            foreach (string testCase in testCases)
+            {
+                using (var istream = new StringReader(testCase))
+                using (var ostream = new StringWriter())
+                {
+                    string source = File.ReadAllText(Path.Combine(TestSourcesPath, "rot13.bf"));
+                    var settings = new BrainSharp.InterpreterSettings();
+                    settings.InputStream = istream;
+                    settings.OutputStream = ostream;
+                    var interpreter = new BrainSharp.Interpreter(source, settings);
+                    interpreter.Execute();
+                    Assert.AreEqual("Uryyb Jbeyq!", settings.OutputStream.ToString(), $"Input: {testCase} Output: {settings.OutputStream}");
+                }
+            }
+        }
+    }
+}
